10cc8076ba2832c885a703c428ccb7b342730d38
prabhat pushp <46195662+prabhatpushp@users.noreply.github.com>
2020-04-06 18:13:37 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,184 @@
+let statedistrictlist = { "Andaman and Nicobar Islands": ["Nicobars","North and Middle Andaman","South Andaman"],"Andhra Pradesh": ["Anantapur","Chittoor","East Godavari","Guntur","Krishna","Kurnool","Prakasam","S.P.S. Nellore","Srikakulam","Visakhapatnam","Vizianagaram","West Godavari","Y.S.R."],"Arunachal Pradesh": ["Anjaw","Changlang","East Kameng","East Siang","Kamle","Kra Daadi","Kurung Kumey","Lepa Rada","Lohit","Longding","Lower Dibang Valley","Lower Siang","Lower Subansiri","Namsai","Pakke Kessang","Papum Pare","Shi Yomi","Siang","Tawang","Tirap","Upper Dibang Valley","Upper Siang","Upper Subansiri","West Kameng","West Siang"],"Assam": ["Baksa","Barpeta","Biswanath","Bongaigaon","Cachar","Charaideo","Chirang","Darrang","Dhemaji","Dhubri","Dibrugarh","Dima Hasao","Goalpara","Golaghat","Hailakandi","Hojai","Jorhat","Kamrup","Kamrup Metropolitan","Karbi Anglong","Karimganj","Kokrajhar","Lakhimpur","Majuli","Morigaon","Nagaon","Nalbari","Sivasagar","Sonitpur","South Salmara Mancachar","Tinsukia","Udalguri","West Karbi Anglong"],"Bihar": ["Araria","Arwal","Aurangabad","Banka","Begusarai","Bhagalpur","Bhojpur","Buxar","Darbhanga","Gaya","Gopalganj","Jamui","Jehanabad","Kaimur Bhabua","Katihar","Khagaria","Kishanganj","Lakhisarai","Madhepura","Madhubani","Munger","Muzaffarpur","Nalanda","Nawada","West Champaran","Patna","East Champaran","Purnia","Rohtas","Saharsa","Samastipur","Saran","Sheikhpura","Sheohar","Sitamarhi","Siwan","Supaul","Vaishali"],"Chandigarh": ["Chandigarh"],"Chhattisgarh": ["Balod","Baloda Bazar","Balrampur","Bametara","Bastar","Bijapur","Bilaspur","Dakshin Bastar Dantewada","Dhamtari","Durg","Gariaband","Janjgir Champa","Jashpur","Kabeerdham","Kondagaon","Korba","Koriya","Mahasamund","Mungeli","Narayanpur","Raigarh","Raipur","Rajnandgaon","Sukma","Surajpur","Surguja","Uttar Bastar Kanker"],"Dadra and Nagar Haveli": ["Dadra and Nagar Haveli"],"Daman And Diu": ["Daman","Diu"],"Delhi": ["Central Delhi","East Delhi","New Delhi","North Delhi","North East Delhi","North West Delhi","Shahdara","South Delhi","South East Delhi","South West Delhi","West Delhi"],"Goa": ["North Goa","South Goa"],"Gujarat": ["Ahmadabad","Amreli","Anand","Aravalli","Banas Kantha","Bharuch","Bhavnagar","Botad","Chota Udaipur","Devbhumi Dwarka","Dohad","Gandhinagar","Gir Somnath","Jamnagar","Junagadh","Kachchh","Kheda","Mahesana","Mahisagar","Morbi","Narmada","Navsari","Panch Mahals","Patan","Porbandar","Rajkot","Sabar Kantha","Surat","Surendranagar","Tapi","The Dangs","Vadodara","Valsad"],"Haryana": ["Ambala","Bhiwani","Charki Dadri","Faridabad","Fatehabad","Gurugram","Hisar","Jhajjar","Jind","Kaithal","Karnal","Kurukshetra","Mahendragarh","Nuh","Palwal","Panchkula","Panipat","Rewari","Rohtak","Sirsa","Sonipat","Yamunanagar"],"Himachal Pradesh": ["Bilaspur","Chamba","Hamirpur","Kangra","Kinnaur","Kullu","Lahul and Spiti","Mandi","Shimla","Sirmaur","Solan","Una"],"Jammu and Kashmir": ["Anantnag","Badgam","Bandipore","Baramula","Doda","Ganderbal","Jammu","Kathua","Kishtwar","Kulgam","Kupwara","Mirpur","Muzaffarabad","Pulwama","Punch","Rajouri","Ramban","Reasi","Samba","Shupiyan","Srinagar","Udhampur"],"Jharkhand": ["Bokaro","Chatra","Deoghar","Dhanbad","Dumka","Garhwa","Giridih","Godda","Gumla","Hazaribagh","Jamtara","Khunti","Kodarma","Latehar","Lohardaga","Pakur","Palamu","Pashchimi Singhbhum","Purbi Singhbhum","Ramgarh","Ranchi","Sahibganj","Saraikela-Kharsawan","Simdega"],"Karnataka": ["Bagalkote","Ballari","Belagavi","Bengaluru","Bengaluru Rural","Bidar","Chamarajanagara","Chikkaballapura","Chikkamagaluru","Chitradurga","Dakshina Kannada","Davanagere","Dharwad","Gadag","Hassan","Haveri","Kalaburagi","Kodagu","Kolar","Koppal","Mandya","Mysuru","Raichur","Ramanagara","Shivamogga","Tumakuru","Udupi","Uttara Kannada","Vijayapura","Yadgir"],"Kerala": ["Alappuzha","Ernakulam","Idukki","Kannur","Kasaragod","Kollam","Kottayam","Kozhikode","Malappuram","Palakkad","Pathanamthitta","Thiruvananthapuram","Thrissur","Wayanad"],"Ladakh": ["Kargil","Leh"],"Lakshadweep": ["Lakshadweep"],"Madhya Pradesh": ["Agar Malwa","Alirajpur","Anuppur","Ashoknagar","Balaghat","Barwani","Betul","Bhind","Bhopal","Burhanpur","Chhatarpur","Chhindwara","Damoh","Datia","Dewas","Dhar","Dindori","Khandwa","Guna","Gwalior","Harda","Hoshangabad","Indore","Jabalpur","Jhabua","Katni","Mandla","Mandsaur","Morena","Narsimhapur","Neemuch","Niwari","Panna","Raisen","Rajgarh","Ratlam","Rewa","Sagar","Satna","Sehore","Seoni","Shahdol","Shajapur","Sheopur","Shivpuri","Sidhi","Singrauli","Tikamgarh","Ujjain","Umaria","Vidisha","Khargone"],"Maharashtra": ["Ahmadnagar","Akola","Amravati","Aurangabad","Bhandara","Bid","Buldana","Chandrapur","Dhule","Gadchiroli","Gondiya","Hingoli","Jalgaon","Jalna","Kolhapur","Latur","Mumbai","Mumbai Suburban","Nagpur","Nanded","Nandurbar","Nashik","Osmanabad","Palghar","Parbhani","Pune","Raigarh","Ratnagiri","Sangli","Satara","Sindhudurg","Solapur","Thane","Wardha","Washim","Yavatmal"],"Manipur": ["Bishnupur","Chandel","Churachandpur","Imphal East","Imphal West","Jiribam","Kakching","Kamjong","Kangpokpi","Noney","Pherzawl","Senapati","Tamenglong","Tengnoupal","Thoubal","Ukhrul"],"Meghalaya": ["East Garo Hills","East Jaintia Hills","East Khasi Hills","North Garo Hills","Ribhoi","South Garo Hills","South West Garo Hills","South West Khasi Hills","West Garo Hills","West Jaintia Hills","West Khasi Hills"],"Mizoram": ["Aizawl","Champhai","Kolasib","Lawngtlai","Lunglei","Mamit","Saiha","Serchhip"],"Nagaland": ["Dimapur","Kiphire","Kohima","Longleng","Mokokchung","Mon","Peren","Phek","Tuensang","Wokha","Zunheboto"],"Odisha": ["Anugul","Balangir","Baleshwar","Bargarh","Baudh","Bhadrak","Cuttack","Debagarh","Dhenkanal","Gajapati","Ganjam","Jagatsinghapur","Jajapur","Jharsuguda","Kalahandi","Kandhamal","Kendrapara","Kendujhar","Khordha","Koraput","Malkangiri","Mayurbhanj","Nabarangapur","Nayagarh","Nuapada","Puri","Rayagada","Sambalpur","Subarnapur","Sundargarh"],"Puducherry": ["Karaikal","Mahe","Puducherry","Yanam"],"Punjab": ["Amritsar","Barnala","Bathinda","Faridkot","Fatehgarh Sahib","Fazilka","Firozpur","Gurdaspur","Hoshiarpur","Jalandhar","Kapurthala","Ludhiana","Mansa","Moga","Pathankot","Patiala","Rupnagar","S.A.S. Nagar","Sangrur","Shahid Bhagat Singh Nagar","Sri Muktsar Sahib","Tarn Taran"],"Rajasthan": ["Ajmer","Alwar","Banswara","Baran","Barmer","Bharatpur","Bhilwara","Bikaner","Bundi","Chittaurgarh","Churu","Dausa","Dhaulpur","Dungarpur","Ganganagar","Hanumangarh","Jaipur","Jaisalmer","Jalore","Jhalawar","Jhunjhunu","Jodhpur","Karauli","Kota","Nagaur","Pali","Pratapgarh","Rajsamand","Sawai Madhopur","Sikar","Sirohi","Tonk","Udaipur"],"Sikkim": ["East District","North District","South District","West District"],"Tamil Nadu": ["Ariyalur","Chennai","Coimbatore","Cuddalore","Dharmapuri","Dindigul","Erode","Kancheepuram","Kanniyakumari","Karur","Krishnagiri","Madurai","Nagapattinam","Namakkal","Perambalur","Pudukkottai","Ramanathapuram","Salem","Sivaganga","Thanjavur","The Nilgiris","Theni","Thiruvallur","Thiruvarur","Thoothukkudi","Tiruchirappalli","Tirunelveli","Tiruppur","Tiruvannamalai","Vellore","Viluppuram","Virudhunagar"],"Telangana": ["Adilabad","Bhadradri Kothagudem","Hyderabad","Jagitial","Jangoan","Jayashankar","Jogulamba Gadwal","Kamareddy","Karimnagar","Khammam","Kumuram Bheem Asifabad","Mahabubabad","Mahabubnagar","Mancherial","Medak","Medchal Malkajgiri","Mulugu","Nagarkurnool","Nalgonda","Narayanpet","Nirmal","Nizamabad","Peddapalli","Rajanna Sircilla","Ranga Reddy","Sangareddy","Siddipet","Suryapet","Vikarabad","Wanaparthy","Warangal Rural","Warangal Urban","Yadadri Bhuvanagiri"],"Tripura": ["Dhalai","Gomati","Khowai","North Tripura","Sipahijala","South Tripura","Unokoti","West Tripura"],"Uttar Pradesh": ["Agra","Aligarh","Ambedkar Nagar","Amethi","Amroha","Auraiya","Azamgarh","Baghpat","Bahraich","Ballia","Balrampur","Banda","Bara Banki","Bareilly","Basti","Bhadohi","Bijnor","Budaun","Bulandshahr","Chandauli","Chitrakoot","Deoria","Etah","Etawah","Faizabad","Farrukhabad","Fatehpur","Firozabad","Gautam Buddha Nagar","Ghaziabad","Ghazipur","Gonda","Gorakhpur","Hamirpur","Hapur","Hardoi","Hathras","Jalaun","Jaunpur","Jhansi","Kannauj","Kanpur Dehat","Kanpur Nagar","Kasganj","Kaushambi","Kheri","Kushinagar","Lalitpur","Lucknow","Mahoba","Mahrajganj","Mainpuri","Mathura","Mau","Meerut","Mirzapur","Moradabad","Muzaffarnagar","Pilibhit","Pratapgarh","Prayagraj","Rae Bareli","Rampur","Saharanpur","Sambhal","Sant Kabir Nagar","Shahjahanpur","Shamli","Shrawasti","Siddharthnagar","Sitapur","Sonbhadra","Sultanpur","Unnao","Varanasi"],"Uttarakhand": ["Almora","Bageshwar","Chamoli","Champawat","Dehradun","Haridwar","Nainital","Pauri Garhwal","Pithoragarh","Rudraprayag","Tehri Garhwal","Udham Singh Nagar","Uttarkashi"],"West Bengal": ["Alipurduar","Bankura","Birbhum","Cooch Behar","Dakshin Dinajpur","Darjeeling","Hooghly","Howrah","Jalpaiguri","Jhargram","Kalimpong","Kolkata","Maldah","Medinipur West","Murshidabad","Nadia","North 24 Parganas","Paschim Bardhaman","Purba Bardhaman","Medinipur East","Purulia","South 24 Parganas","Uttar Dinajpur"]};
+let state = window.localStorage.getItem('state'),district=window.localStorage.getItem('district');
+
+
+// hide navbar on scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let width = window.innerWidth;
+  if(width <=600){
+    var currentScrollPos = window.pageYOffset;
+    var navbar = document.getElementById("navbar");
+    var topbar = document.getElementById("topbar");
+    if (prevScrollpos > currentScrollPos) {
+      navbar.style.bottom = "0";
+      topbar.style.top = "0";
+    } else {
+      navbar.style.bottom = "-4rem";
+      topbar.style.top = `-${topbar.offsetHeight}px`;
+    }
+    prevScrollpos = currentScrollPos;
+  }
+}
+//open settings
+function openSettings(){
+  let settingsDiv = document.querySelector('.settings');
+  settingsDiv.classList.toggle('shide')
+}
+
+
+// fetching json data
+function handleErrors(response) {
+  if (!response.ok) {
+      throw Error(response.statusText);
+  }
+  return response;
+}
+
+function requestJSON(url){
+  return fetch(url)
+  .then(handleErrors)
+  .then(response => response.json())
+  .catch(error => console.log(error) );
+}    
+
+// Data inserter to dom
+function pushDatatoDOM(id,data,clear=false){
+  let element = document.getElementById(id);
+  if(clear){
+      element.innerHTML = data;
+  }else{
+      element.innerHTML += data;
+  }
+}
+
+
+function pushDistrict(){
+
+}
+
+//settings section
+window.onload = ()=>{
+  //State Dropdown
+  state = window.localStorage.getItem('state');
+  const stateField = document.getElementById('state-value');
+  const statedropdown = document.getElementById('state-list');
+  Object.keys(statedistrictlist).forEach(state => {
+    statedropdown.innerHTML += `<li>${state}</li>`;
+  })
+  const statedropdownArray = [...statedropdown.querySelectorAll('li')];
+  let stateArray = [];
+  statedropdownArray.forEach(item => {
+    stateArray.push(item.textContent);
+  });
+
+  stateField.addEventListener('input', () => {
+    statedropdown.classList.add('open');
+    let inputValue = stateField.value.toLowerCase();
+    if (inputValue.length > 0) {
+      for (let j = 0; j < stateArray.length; j++) {
+        if (!(inputValue.substring(0, inputValue.length) === stateArray[j].substring(0, inputValue.length).toLowerCase())) {
+          statedropdownArray[j].classList.add('closed');
+        } else {
+          statedropdownArray[j].classList.remove('closed');
+        }
+      }
+    } else {
+      for (let i = 0; i < statedropdownArray.length; i++) {
+        statedropdownArray[i].classList.remove('closed');
+      }
+    }
+  });
+
+  statedropdownArray.forEach(item => {
+    item.addEventListener('click', evt => {
+      stateField.value = item.textContent;
+      window.localStorage.setItem('state',item.textContent);
+      state = item.textContent;
+      pushDistrict();
+      statedropdownArray.forEach(statedropdown => {
+        statedropdown.classList.add('closed');
+      });
+    });
+  });
+
+  stateField.addEventListener('focus', () => {
+    stateField.placeholder = 'Type State';
+    statedropdown.classList.add('open');
+    statedropdownArray.forEach(statedropdown => {
+      statedropdown.classList.remove('closed');
+    });
+  });
+
+  document.addEventListener('click', evt => {
+    const isstatedropdown = statedropdown.contains(evt.target);
+    const isInput = stateField.contains(evt.target);
+    if (!isstatedropdown && !isInput) {
+      statedropdown.classList.remove('open');
+    }
+  });
+
+
+  //District Dropdown
+  function pushDistrict(){
+    const districtField = document.getElementById('district-value');
+    const districtdropdown = document.getElementById('district-list');
+    if(statedistrictlist[state].indexOf(districtField.value) <0){
+      districtField.value = "";
+    }
+    districtdropdown.innerHTML = "";
+    statedistrictlist[state].forEach(districtName=>{
+      districtdropdown.innerHTML += `<li>${districtName}</li>`;
+    })
+    const districtdropdownArray = [...districtdropdown.querySelectorAll('li')];
+    let districtArray = [];
+    districtdropdownArray.forEach(item => {
+      districtArray.push(item.textContent);
+    });
+  
+    districtField.addEventListener('input', () => {
+      districtdropdown.classList.add('open');
+      let inputValue = districtField.value.toLowerCase();
+      if (inputValue.length > 0) {
+        for (let j = 0; j < districtArray.length; j++) {
+          if (!(inputValue.substring(0, inputValue.length) === districtArray[j].substring(0, inputValue.length).toLowerCase())) {
+            districtdropdownArray[j].classList.add('closed');
+          } else {
+            districtdropdownArray[j].classList.remove('closed');
+          }
+        }
+      } else {
+        for (let i = 0; i < districtdropdownArray.length; i++) {
+          districtdropdownArray[i].classList.remove('closed');
+        }
+      }
+    });
+  
+    districtdropdownArray.forEach(item => {
+      item.addEventListener('click', evt => {
+        districtField.value = item.textContent;
+        window.localStorage.setItem('district',item.textContent);
+        district = item.textContent;
+        districtdropdownArray.forEach(districtdropdown => {
+          districtdropdown.classList.add('closed');
+        });
+      });
+    });
+  
+    districtField.addEventListener('focus', () => {
+      districtField.placeholder = 'Type District';
+      districtdropdown.classList.add('open');
+      districtdropdownArray.forEach(districtdropdown => {
+        districtdropdown.classList.remove('closed');
+      });
+    });
+  
+    document.addEventListener('click', evt => {
+      const isdistrictdropdown = districtdropdown.contains(evt.target);
+      const isInput = districtField.contains(evt.target);
+      if (!isdistrictdropdown && !isInput) {
+        districtdropdown.classList.remove('open');
+      }
+    });
+  }
+}
---
+++ assets/js/stats.js
@@ -0,0 +1,209 @@
+
+let indianData = null;
+let stateData = null;
+let districtData = null;
+
+function worldDailyStats(data){
+    let totalConfirmed = [];
+    let dailyConfirmed = [];
+    let deceased = [];
+    data.forEach((row,i)=>{
+            totalConfirmed.push({ x: new Date(`${row.reportDate}`) , y: parseInt(row.totalConfirmed)})
+            dailyConfirmed.push({ x: new Date(`${row.reportDate}`) , y: parseInt(row.deltaConfirmed) })
+            deceased.push({ x: new Date(`${row.reportDate}`) , y: parseInt(row.deaths.total) })
+    });
+
+    var chart = new CanvasJS.Chart("world-stat", {
+        animationEnabled: true,
+        theme: "light2",
+        axisX:{
+            valueFormatString: "DD MMM",
+            crosshair: {
+                enabled: true,
+                snapToDataPoint: true
+            }
+        },
+        axisY: {
+            minimum: -10,
+            crosshair: {
+                enabled: false
+            }
+        },
+        toolTip:{
+            shared:true
+        },  
+        legend:{
+            cursor:"pointer",
+            margin :30,
+            dockInsidePlotArea: false,
+            itemclick: toogleDataSeries
+        },
+        data: [{
+            type: "spline",
+            showInLegend: true,
+            name: "Total Deaths",
+            xValueFormatString: "DD MMM, YYYY",
+            color: "#6c757d",
+            dataPoints: deceased
+        },{
+            type: "spline",
+            showInLegend: true,
+            name: "Confirmed",
+            xValueFormatString: "DD MMM, YYYY",
+            color: "#F08080",
+            dataPoints: dailyConfirmed
+        },{
+            type: "spline",
+            showInLegend: true,
+            name: "Total Confirmed",
+            xValueFormatString: "DD MMM, YYYY",
+            color: "#ff073a",
+            dataPoints: totalConfirmed
+        }]
+    });
+    chart.render();
+    function toogleDataSeries(e){
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        } else{
+            e.dataSeries.visible = true;
+        }
+        chart.render();
+    }
+}    
+
+function indianDailyStats(data){
+    let totalConfirmed = [];
+    let dailyConfirmed = [];
+    let dailyRecovered = [];
+    let dailyDeceased = [];
+    let totalDeceased = [];
+    let deaths = 0;
+    data.forEach((row,i)=>{
+        deaths += parseInt(row.dailydeceased);
+        totalConfirmed.push({ x: new Date(`${row.date} 2020`) , y: parseInt(row.totalconfirmed)})
+        dailyConfirmed.push({ x: new Date(`${row.date} 2020`) , y: parseInt(row.dailyconfirmed) })
+        dailyDeceased.push({ x: new Date(`${row.date} 2020`) , y: parseInt(row.dailydeceased) })
+        totalDeceased.push({ x: new Date(`${row.date} 2020`) , y: deaths })
+        dailyRecovered.push({ x: new Date(`${row.date} 2020`) , y: parseInt(row.dailyrecovered) })
+    });
+
+    var chart = new CanvasJS.Chart("indian-stat", {
+        animationEnabled: true,
+        theme: "light2",
+        axisX:{
+            valueFormatString: "DD MMM",
+            crosshair: {
+                enabled: true,
+                snapToDataPoint: true
+            }
+        },
+        axisY: {
+            minimum: -10,
+            crosshair: {
+                enabled: false
+            }
+        },
+        toolTip:{
+            shared:true
+        },  
+        legend:{
+            cursor:"pointer",
+            margin :30,
+            dockInsidePlotArea: false,
+            itemclick: toogleDataSeries
+        },
+        data: [{
+            type: "spline",
+            showInLegend: true,
+            name: "Recovered",
+            xValueFormatString: "DD MMM, YYYY",
+            color: "#28a745",
+            dataPoints: dailyRecovered
+        },{
+            type: "spline",
+            showInLegend: true,
+            name: "Deaths",
+            xValueFormatString: "DD MMM, YYYY",
+            color: "#6c757d",
+            dataPoints: dailyDeceased
+        },{
+            type: "spline",
+            showInLegend: true,
+            name: "Total Deaths",
+            xValueFormatString: "DD MMM, YYYY",
+            color: "#444444",
+            dataPoints: totalDeceased
+        },{
+            type: "spline",
+            showInLegend: true,
+            name: "Confirmed",
+            xValueFormatString: "DD MMM, YYYY",
+            color: "#F08080",
+            dataPoints: dailyConfirmed
+        },{
+            type: "spline",
+            showInLegend: true,
+            name: "Total Confirmed",
+            xValueFormatString: "DD MMM, YYYY",
+            color: "#ff073a",
+            dataPoints: totalConfirmed
+        }]
+    });
+    chart.render();
+    function toogleDataSeries(e){
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        } else{
+            e.dataSeries.visible = true;
+        }
+        chart.render();
+    }
+}    
+// States Stat
+function stateStats(stateName,data){
+    if(data != null){
+        if(stateName != state){
+            state = stateName;
+        }
+        let row = data.find(o => o.state === stateName);
+        pushDatatoDOM('state-name',`${row.state} COVID-19 Status`,true)
+        pushDatatoDOM('state-info',`<li>Confirmed : ${row.confirmed}</li>`,true)
+        pushDatatoDOM('state-info',`<li>Active : ${row.active}</li>`)
+        pushDatatoDOM('state-info',`<li>Recovered : ${row.recovered}</li>`)
+        pushDatatoDOM('state-info',`<li>Deaths : ${row.deaths}</li>`)
+    }else{ console.log("Some Error occured!") }
+}
+// District Stat
+function districtStats(districtName,data){
+
+    if(districtName != district){
+        district = districtName;
+    }
+    if(data != null){
+        let row = data[state]["districtData"][district];
+        pushDatatoDOM('district-name',`${district} COVID-19 Status`,true)
+        pushDatatoDOM('district-info',`<li>Confirmed : ${row.confirmed}</li>`,true)
+    }else{ console.log("Some Error occured!") }
+}
+
+window.onload = ()=>{
+    // Indian Stat
+    requestJSON("https://api.covid19india.org/data.json")
+                    .then((data)=>{
+                        stateData = data.statewise;
+                        indianData = data.cases_time_series;
+                        stateStats(state,stateData);
+                        indianDailyStats(indianData);
+                    });
+    //District Stat
+    requestJSON("https://api.covid19india.org/state_district_wise.json")
+                    .then((data)=>{
+                        districtData=data;
+                        districtStats(district,districtData);
+                    });
+    requestJSON("https://covid19.mathdro.id/api/daily")
+                    .then((data)=>{
+                        worldDailyStats(data);
+                    });
+}
