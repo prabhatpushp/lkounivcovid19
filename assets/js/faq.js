1b726f861e77b8faf93690fca7c1646781de3f64
prabhat pushp <46195662+prabhatpushp@users.noreply.github.com>
2020-04-06 18:56:10 +0530

    faq responsive

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -2,5 +2,5 @@
 window.onload = function() {
     let myiFrame = document.getElementById("twitter-widget-0");
     let doc = myiFrame.contentDocument;
-    doc.body.innerHTML = doc.body.innerHTML + '<style>.timeline-Header-subtitle{display: none !important;}.timeline-Header-description{display: none !important;}</style>';
+    doc.body.innerHTML = doc.body.innerHTML + '<style>.timeline-Header{position:sticky;top:0;}.timeline-Header-subtitle{display: none !important;}.timeline-Header-description{display: none !important;}</style>';
 } 
